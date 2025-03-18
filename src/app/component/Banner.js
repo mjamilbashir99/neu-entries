0e5e10ffcfcec4b3a3bab5675e36e2d4b1ea2953
Jamil Bashir <jamil@themindgauge.com>
2025-03-18 21:41:07 +0500

    Removed banner

--- src/app/component/Banner.js
+++ src/app/component/Banner.js
@@ -1,21 +1,21 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { Instrument_Serif } from "next/font/google";
-import { Inter } from "next/font/google";
+// import { Instrument_Serif } from "next/font/google";
+// import { Inter } from "next/font/google";
 import ToggleStartBtn from "./ToggleStartBtn";
 import Link from "next/link";
 import Image from "next/image";
 
-const instrumentSerif = Instrument_Serif({
-  subsets: ["italic"],
-  display: "swap",
-  weight: "400",
-});
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  weight: "500",
-});
+// const instrumentSerif = Instrument_Serif({
+//   subsets: ["italic"],
+//   display: "swap",
+//   weight: "400",
+// });
+// const inter = Inter({
+//   subsets: ["latin"],
+//   display: "swap",
+//   weight: "500",
+// });
 
 const Banner = ({ img, title1, title2, title3, title4 }) => {
   return (
@@ -30,16 +30,16 @@ const Banner = ({ img, title1, title2, title3, title4 }) => {
 
       {/* Content with higher z-index */}
       <div className="w-2/6 mx-auto mt-12 z-20">
-        <p className={`font-bold text-[60px] text-center ${inter.className}`}>
+        <p className={`font-bold text-[60px] text-center`}>
           {title1}
         </p>
-        <p className={`text-[60px] text-center  ${instrumentSerif.className}`}>
+        <p className={`text-[60px] text-center`}>
           {title2}
         </p>
-        <p className={`font-bold text-[60px] text-center  ${inter.className}`}>
+        <p className={`font-bold text-[60px] text-center`}>
           {title3} {/* Ensure inter font is applied here */}
         </p>
-        <p className={`mt-10 text-[20px] text-center  ${inter.className}`}>
+        <p className={`mt-10 text-[20px] text-center`}>
           {title4}
         </p>
       </div>
