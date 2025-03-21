eecf1610fad20334db997d08221f2c334a4ec95f
MuhammadSufyanDevelopment <dhdhdudhshshs@gmail.com>
2025-03-20 23:10:16 -0700

    added space for testing

--- src/app/journal/page.js
+++ src/app/journal/page.js
@@ -9,6 +9,7 @@ import { MdChevronRight } from "react-icons/md";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
+
 export default function Home() {
   const router = useRouter();
   const [currentDate, setCurrentDate] = useState("");
